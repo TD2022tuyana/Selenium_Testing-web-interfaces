8d1f1cb4d35e9ce7865a3a9028a9186efcba004f
TD2022tuyana <112084157+TD2022tuyana@users.noreply.github.com>
2023-03-12 16:20:27 +0300

    Add CardTests

--- src/test/java/OrderingCardTest.java
+++ src/test/java/OrderingCardTest.java
@@ -3,9 +3,12 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class OrderingCardTest {
     WebDriver driver;
@@ -14,7 +17,6 @@ public class OrderingCardTest {
     static void setupAll() {
         WebDriverManager.chromedriver().setup();
     }
-
     @BeforeEach
     void setup() {
         ChromeOptions options = new ChromeOptions();
@@ -22,6 +24,7 @@ public class OrderingCardTest {
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
+        driver.get("http://localhost:9999");
 
     }
 
@@ -29,11 +32,83 @@ public class OrderingCardTest {
     @AfterEach
     void teardown() {
         driver.quit();
+        driver = null;
     }
 
     @Test
-    void test() {
-        driver.get("http://localhost:9999");
-        // Your test logic here
+    void testFormSubmission() {
+        // Find the form fields and fill them in
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Шарапов Дмитрий");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+71234567890");
+
+        // Check the terms and conditions checkbox
+        driver.findElement(By.className("checkbox__box")).click();
+
+        // Submit the form
+        driver.findElement(By.className("button")).click();
+
+        // Verify that the success message is displayed
+        String actual = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", actual);
+    }
+
+    @Test
+    void testNameWithDash() {
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Михаил Салтыков-Щедрин");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+71234567890");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button")).click();
+        String actual = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", actual);
+    }
+
+    @Test
+    void testNameWithApostrophe() {
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Жанна Д'Арк");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+71234567890");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button")).click();
+        String actualNameError = driver.findElement(By.cssSelector("[data-test-id='name'].input_invalid .input__sub")).getText().trim();
+        assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", actualNameError);
+    }
+
+    @Test
+    void testShortName() {
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Ян Лао");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+71234567890");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button")).click();
+        String actual = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", actual);
+    }
+
+    @Test
+    void testInvalidFormSubmission() {
+        // Find the form fields and fill them in with invalid data
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Ivan");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("1234567890");
+
+        // Check the terms and conditions checkbox
+        driver.findElement(By.className("checkbox__box")).click();
+
+        // Submit the form
+        driver.findElement(By.className("button")).click();
+
+        // Verify that the error message is displayed
+        String actualNameError = driver.findElement(By.cssSelector("[data-test-id='name'].input_invalid .input__sub")).getText().trim();
+        assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", actualNameError);
+
+    }
+
+    @Test
+    void testInvalidFormSubmissionNumber() {
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Иван Иванов");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("1234567890");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button")).click();
+
+        String actualPhoneError = driver.findElement(By.cssSelector("[data-test-id='phone'].input_invalid .input__sub")).getText().trim();
+        assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", actualPhoneError);
+
     }
 }
